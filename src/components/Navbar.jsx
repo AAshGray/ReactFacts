f3a7d46b2af21e25d5768a2c9d7b6d609599f841
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-09-28 16:15:34 -0700

    fixed header  image  to use public directory

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+
+
 export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="src\assets\react-logo.png" className="nav-logo" alt="React logo" />
+                <img src="/react-logo.png" className="nav-logo" alt="React logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
