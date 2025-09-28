bfa734532fe26c771dc6352084e60413d9fa8202
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-09-28 16:45:58 -0700

    trying again with asset import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import logo from '../assets/react-logo.png';
+
 export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="/react-logo.png" className="nav-logo" alt="React logo" />
+                <img src={logo} className="nav-logo" alt="React logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
