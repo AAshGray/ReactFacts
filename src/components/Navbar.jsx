984a7a044af7e41b5438f2f04cce07f7cba66994
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-09-28 16:28:58 -0700

    trying accessing public  again but differently

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
-import logo from "src/assets/react-logo.png"
-
 export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src={logo} className="nav-logo" alt="React logo" />
+                <img src="/react-logo.png" className="nav-logo" alt="React logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
